038eed6f9b3e8bb2198578e65239ced39b03b865
Mats Andresen <m.andresen@epages.com>
2017-11-12 21:01:04 +0100

    initial commit

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var axios = require('axios');
+
+describe('API', function() {
+  describe('tests', function() {
+    it('should be implemented', function() {
+        assert.equal(1, 1)
+    });
+  });
+});
